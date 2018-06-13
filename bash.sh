4afc3d4fc40c7a3716b36bdeab6db26879c4da33
nNevermore <bartosz.j.wojtowicz@gmail.com>
2018-06-13 10:11:12 +0200

    new file:   bash.sh 	modified:   www/templates/www/post_list.html 	modified:   www/views.py

---
+++ bash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'PS1="\e[1m\u:\e[34m\w\e[31m$(__git_ps1)\e[92m\e[0m\$ "'
--- www/views.py
+++ www/views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+# def post_list(request):
+#      return render(request, 'www/post_list.html', {})
+
+# Create your views here.
+
 def post_list(request):
-    return render(request, 'www/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    ppl = Post.objects.all();
+    #posts = Post.objects
+    return render(request, 'www/post_list.html', {'posty': posts, 'ppl': ppl})
